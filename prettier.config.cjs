4d6c413f21663884a69a912371bbbb509afb1611
jbreit <jason.breitigan@gmail.com>
2024-05-12 19:17:00 -0400

    build(prettier.config.cjs): add config
    Added prettier.config.cjs file

---
+++ prettier.config.cjs
@@ -0,0 +1,65 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  endOfLine: 'auto',
+  experimentalTernaries: false,
+  insertPragma: false,
+  jsxSingleQuote: false,
+  overrides: [
+    {
+      files: ['.editorconfig', '.npmrc'],
+      options: {
+        iniSpaceAroundEquals: true,
+        parser: 'ini',
+      },
+    },
+    {
+      files: ['**/*.html', '**/*.htm'],
+      options: {
+        embeddedLanguageFormatting: 'auto',
+        // https://prettier.io/blog/2018/11/07/1.15.0.html#whitespace-sensitive-formatting
+        htmlWhitespaceSensitivity: 'css',
+        parser: 'html',
+        printWidth: 9999,
+        singleAttributePerLine: true,
+        trailingComma: 'none',
+      },
+    },
+    {
+      files: ['LICENSE', '**/*.md'],
+      options: {
+        embeddedLanguageFormatting: 'auto',
+        parser: 'markdown',
+        proseWrap: 'preserve',
+      },
+    },
+    {
+      files: ['**/*.sh'],
+      options: {
+        /**
+         * parser options
+         */
+        keepComments: true,
+        parser: 'sh',
+        /**
+         * printer options
+         */
+        binaryNextLine: true,
+        indent: 2,
+        minify: false,
+      },
+    },
+  ],
+  plugins: ['prettier-plugin-ini', 'prettier-plugin-sh', 'prettier-plugin-sql'],
+  printWidth: 80,
+  quoteProps: 'as-needed',
+  rangeStart: 0,
+  rangeEnd: Infinity,
+  requirePragma: false,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
+};
