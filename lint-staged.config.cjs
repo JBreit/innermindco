2f11870ae0c10a8b29a3c51014f4f09e48d23111
jbreit <jason.breitigan@gmail.com>
2024-05-12 19:17:35 -0400

    build(lint-staged.config.cjs): add config
    Added lint-staged.config.cjs file

---
+++ lint-staged.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  'src/**/*.{cjs,cts,mjs,mts,js,jsx,ts,tsx,vue}': [
+    'npm run prettier:format',
+    'npm run lint:fix',
+  ],
+  '*.md': ['npm run mdlint:fix'],
+  '.editorconfig': [
+    'npx prettier --write .editorconfig --plugin=prettier-plugin-ini',
+  ],
+  LICENSE: ['npx prettier --write'],
+  'package.json': [
+    'npx prettier --config prettier.config.js ./package.json --write',
+    'npm run prettier:package',
+  ],
+};
